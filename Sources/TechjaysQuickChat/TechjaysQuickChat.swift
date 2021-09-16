9f768e1e024def4e3d4fd28e74654d00d3059138
SathyaKrishna <sathya.gr@iderize.in>
2021-09-16 12:56:32 +0530

    Navigation commit

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -1,5 +1,18 @@
+import UIKit
+
 public struct TechjaysQuickChat {
     var text = "Hello, World!"
+    var topMostController: UIViewController? {
+        guard let win = UIApplication.shared.windows.filter({$0.isKeyWindow}).first,
+            let rootViewController = win.rootViewController else {
+            return nil
+        }
+        var topController = rootViewController
+        while let newTopController = topController.presentedViewController {
+            topController = newTopController
+        }
+        return topController
+    }
     
     public init() {
         
@@ -8,4 +21,12 @@ public struct TechjaysQuickChat {
     public func hello() -> String {
         return "Hello"
     }
+    
+    public func openChatListScreen() {
+        let vc = UIStoryboard.initial(storyboard: UserManager().currentUserID().isNone ? .auth : .conversations)
+        vc.modalPresentationStyle = .fullScreen
+        topMostController?.present(vc, animated: true, completion: nil)
+    }
 }
+
+
