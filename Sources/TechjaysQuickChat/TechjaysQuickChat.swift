1ace96068cdf42314c9848648e0a2338b75ec0ec
SathyaKrishna <sathya.gr@iderize.in>
2021-09-16 12:34:56 +0530

    Added a test method

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -1,5 +1,11 @@
 public struct TechjaysQuickChat {
     var text = "Hello, World!"
     
+    public init() {
+        
+    }
     
+    public func hello() -> String {
+        return "Hello"
+    }
 }
