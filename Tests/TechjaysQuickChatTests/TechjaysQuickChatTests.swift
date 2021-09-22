4e9a960c6ece90e1dff41e9d08d8b82f7bfb0c12
SathyaKrishna <sathya.gr@iderize.in>
2021-09-22 11:05:51 +0530

    test case update

--- Tests/TechjaysQuickChatTests/TechjaysQuickChatTests.swift
+++ Tests/TechjaysQuickChatTests/TechjaysQuickChatTests.swift
@@ -6,6 +6,6 @@
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-            XCTAssertEqual(TechjaysQuickChat().text, "Hello, World!")
+            XCTAssertEqual("TechjaysQuickChat()", "Hello, World!")
         }
     }
