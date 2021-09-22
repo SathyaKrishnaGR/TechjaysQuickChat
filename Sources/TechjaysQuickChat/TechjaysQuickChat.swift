4b76d328b6e4658337cab714e94371f5a265926d
SathyaKrishna <sathya.gr@iderize.in>
2021-09-22 10:58:38 +0530

    Params added

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -6,8 +6,8 @@ public struct TechjaysQuickChat {
         
     }
     
-    // User Id, Profile, 
-    public func openChatListScreen() {
+    // AccessToken, ChatToken,
+    public func openChatListScreen(accessToken: String, chatToken: String, appEndPoint: String) {
         let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
         if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
             viewController.modalPresentationStyle = .fullScreen
