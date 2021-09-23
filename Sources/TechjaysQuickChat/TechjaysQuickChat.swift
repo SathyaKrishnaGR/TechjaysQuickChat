5c71bd5870e1d1b9b7776f67f1320e8a2b4cce5f
SathyaKrishna <sathya.gr@iderize.in>
2021-09-23 12:02:37 +0530

    Popover style changed

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -17,27 +17,18 @@ public struct TechjaysQuickChat {
         
         let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
         if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
-//            viewController.modalPresentationStyle = .popover
+            viewController.modalPresentationStyle = .popover
             
             
             print("Chat token \(FayvKeys.ChatDefaults.chatToken)")
             print("AccessToken \(FayvKeys.ChatDefaults.token)")
             print("Endpoint \(FayvKeys.ChatDefaults.endpoint)")
             DispatchQueue.main.async {
-//                topMostController?.present(viewController, animated: true, completion: nil)
-                topMostController?.embed(viewController, inView: (topMostController?.view)!)
+                topMostController?.present(viewController, animated: true, completion: nil)
                 // tell the childviewcontroller it's contained in it's parent
             }
         }
     }
 }
-extension UIViewController {
-    func embed(_ viewController:UIViewController, inView view:UIView){
-        viewController.willMove(toParent: self)
-        viewController.view.frame = view.bounds
-        view.addSubview(viewController.view)
-        self.addChild(viewController)
-        viewController.didMove(toParent: self)
-    }
-}
+
 
