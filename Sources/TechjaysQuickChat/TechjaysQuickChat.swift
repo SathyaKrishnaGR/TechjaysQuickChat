c1c98b7796025cfcac32dd76535f55fb9cb84ed3
SathyaKrishna <sathya.gr@iderize.in>
2021-09-24 00:43:52 +0530

    overContext

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -17,12 +17,11 @@ public struct TechjaysQuickChat {
         
         let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
         if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
-            UINavigationController().pushViewController(viewController, animated: true)
-//            viewController.modalPresentationStyle = .currentContext
-//            DispatchQueue.main.async {
-//                topMostController?.present(viewController, animated: true, completion: nil)
-//                // tell the childviewcontroller it's contained in it's parent
-//            }
+              viewController.modalPresentationStyle = .overCurrentContext
+            DispatchQueue.main.async {
+                topMostController?.present(viewController, animated: true, completion: nil)
+                // tell the childviewcontroller it's contained in it's parent
+            }
         }
     }
     
@@ -36,13 +35,12 @@ public struct TechjaysQuickChat {
           viewController.opponentUserName = opponentUserName
           viewController.userId = userId
           viewController.isFromReel = isFromReel
-//         viewController.modalPresentationStyle = .currentContext
-            UINavigationController().pushViewController(viewController, animated: true)
+         viewController.modalPresentationStyle = .overCurrentContext
 //
-//         DispatchQueue.main.async {
-//          topMostController?.present(viewController, animated: true, completion: nil)
-//          // tell the childviewcontroller it’s contained in it’s parent
-//         }
+         DispatchQueue.main.async {
+          topMostController?.present(viewController, animated: true, completion: nil)
+          // tell the childviewcontroller it’s contained in it’s parent
+         }
         }
        }
 }
