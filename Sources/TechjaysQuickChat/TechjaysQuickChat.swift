4be84e350e896ba9c5aeda95bac67b35beccf7fb
SathyaKrishna <sathya.gr@iderize.in>
2021-09-23 18:33:03 +0530

    API changes

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -17,7 +17,7 @@ public struct TechjaysQuickChat {
         
         let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
         if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
-            viewController.modalPresentationStyle = .popover
+            viewController.modalPresentationStyle = .currentContext
             DispatchQueue.main.async {
                 topMostController?.present(viewController, animated: true, completion: nil)
                 // tell the childviewcontroller it's contained in it's parent
@@ -35,7 +35,7 @@ public struct TechjaysQuickChat {
           viewController.opponentUserName = opponentUserName
           viewController.userId = userId
           viewController.isFromReel = isFromReel
-         viewController.modalPresentationStyle = .popover
+         viewController.modalPresentationStyle = .currentContext
          DispatchQueue.main.async {
           topMostController?.present(viewController, animated: true, completion: nil)
           // tell the childviewcontroller it’s contained in it’s parent
