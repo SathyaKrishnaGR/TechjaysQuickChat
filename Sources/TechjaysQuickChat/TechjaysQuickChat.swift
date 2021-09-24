89b5e3769b9541f057cfd9a181b63c4711f18b1b
SathyaKrishna <sathya.gr@iderize.in>
2021-09-24 11:50:23 +0530

    Fayvit navigation - Method

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -7,43 +7,43 @@ public struct TechjaysQuickChat {
     }
     
     // AccessToken, ChatToken,
-    public func openChatListScreen(accessToken: String, chatToken: String, appEndPoint: String, socket: String) {
-        
-        FayvKeys.ChatDefaults.chatToken = chatToken
-        FayvKeys.ChatDefaults.token = accessToken
-        FayvKeys.ChatDefaults.endpoint = appEndPoint
-        FayvKeys.ChatDefaults.socketUrl = socket
-        
-        
-        let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
-        if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
-              viewController.modalPresentationStyle = .overCurrentContext
-            DispatchQueue.main.async {
-                topMostController?.present(viewController, animated: true, completion: nil)
-                // tell the childviewcontroller it's contained in it's parent
-            }
-        }
-    }
+//    public func openChatListScreen(accessToken: String, chatToken: String, appEndPoint: String, socket: String) {
+//
+//        FayvKeys.ChatDefaults.chatToken = chatToken
+//        FayvKeys.ChatDefaults.token = accessToken
+//        FayvKeys.ChatDefaults.endpoint = appEndPoint
+//        FayvKeys.ChatDefaults.socketUrl = socket
+//
+//
+//        let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
+//        if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
+//              viewController.modalPresentationStyle = .overCurrentContext
+//            DispatchQueue.main.async {
+//                topMostController?.present(viewController, animated: true, completion: nil)
+//                // tell the childviewcontroller it's contained in it's parent
+//            }
+//        }
+//    }
     
-    public func openChatListScreenForVidhire(accessToken: String, chatToken: String, appEndPoint: String, socket: String,isFromReel: Bool?,userId: Int,opponentUserName: String?) {
-        FayvKeys.ChatDefaults.chatToken = chatToken
-        FayvKeys.ChatDefaults.token = accessToken
-        FayvKeys.ChatDefaults.endpoint = appEndPoint
-        FayvKeys.ChatDefaults.socketUrl = socket
-        let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
-        if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
-          viewController.opponentUserName = opponentUserName
-          viewController.userId = userId
-            viewController.isFromReel = isFromReel!
-         viewController.modalPresentationStyle = .overCurrentContext
-            DispatchQueue.main.async {
-                
-                topMostController?.present(viewController, animated: true, completion: nil)
-                        
-            }
-        }
-         
-       }
+//    public func openChatListScreenForVidhire(accessToken: String, chatToken: String, appEndPoint: String, socket: String,isFromReel: Bool?,userId: Int,opponentUserName: String?) {
+//        FayvKeys.ChatDefaults.chatToken = chatToken
+//        FayvKeys.ChatDefaults.token = accessToken
+//        FayvKeys.ChatDefaults.endpoint = appEndPoint
+//        FayvKeys.ChatDefaults.socketUrl = socket
+//        let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
+//        if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
+//          viewController.opponentUserName = opponentUserName
+//          viewController.userId = userId
+//            viewController.isFromReel = isFromReel!
+//         viewController.modalPresentationStyle = .overCurrentContext
+//            DispatchQueue.main.async {
+//
+//                topMostController?.present(viewController, animated: true, completion: nil)
+//
+//            }
+//        }
+//
+//       }
     
     public func presentInContainerWith(accessToken: String, chatToken: String, appEndPoint: String, version: String, socket: String,isFromReel: Bool?,userId: Int,opponentUserName: String?) -> UIViewController {
         FayvKeys.ChatDefaults.chatToken = chatToken
@@ -58,6 +58,24 @@ public struct TechjaysQuickChat {
           viewController.opponentUserName = opponentUserName
           viewController.userId = userId
             viewController.isFromReel = isFromReel!
+            
+            return viewController
+        }
+         return UIViewController()
+       }
+    
+    public func presentInContainerWith(accessToken: String, chatToken: String, appEndPoint: String, version: String, socket: String, userId: Int,opponentUserName: String?) -> UIViewController {
+        FayvKeys.ChatDefaults.chatToken = chatToken
+        FayvKeys.ChatDefaults.token = accessToken
+        FayvKeys.ChatDefaults.endpoint = appEndPoint
+        FayvKeys.ChatDefaults.socketUrl = socket
+        
+        FayvKeys.APIDefaults.baseUrl = appEndPoint
+        FayvKeys.APIDefaults.version = version
+        let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
+        if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
+          viewController.opponentUserName = opponentUserName
+          viewController.userId = userId
             
             return viewController
         }
