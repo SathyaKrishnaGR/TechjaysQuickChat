5bfb4cc8be9cd1e8499625a1c30d1356a2cc10a6
SathyaKrishna <sathya.gr@iderize.in>
2021-09-25 13:48:18 +0530

    LocalNotification

---
+++ Sources/TechjaysQuickChat/QuickChat/LocalNotificationManager.swift
@@ -0,0 +1,49 @@
+//
+//  File.swift
+//  
+//
+//  Created by SathyaKrishna on 25/09/21.
+//
+
+import Foundation
+import UIKit
+import UserNotifications
+
+
+struct LocalNotification {
+    var title: String?
+    var subTitle: String?
+    var body: String?
+}
+
+
+class LocalNotificationManager {
+    
+    static let shared = LocalNotificationManager()
+    
+    func sendNotification(localNotification: LocalNotification) {
+        let content = UNMutableNotificationContent()
+
+        guard let title = localNotification.title else {return}
+        guard let subTitle = localNotification.subTitle else {return}
+        guard let body = localNotification.body else {return}
+        content.title = title
+        content.subtitle = subTitle
+        content.body = body
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
+    func getAccessPermissionAndNotify(localNotification: LocalNotification) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) {
+            (granted, error) in
+            if granted {
+                self.sendNotification(localNotification: localNotification)
+            } else {
+                return
+            }
+        }
+    }
+}
