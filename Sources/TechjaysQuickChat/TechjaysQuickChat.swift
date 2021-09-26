021e29251e2d493b81166e6bd6446fd3622b0ec2
SathyaKrishna <sathya.gr@iderize.in>
2021-09-26 22:48:53 +0530

    ChatNavigation to MessagesController

--- Sources/TechjaysQuickChat/TechjaysQuickChat.swift
+++ Sources/TechjaysQuickChat/TechjaysQuickChat.swift
@@ -1,47 +1,48 @@
 import UIKit
 
 public struct TechjaysQuickChat {
-   
+    
     public init() {
         
     }
-        
-    public func presentInContainerWith(accessToken: String, chatToken: String, appEndPoint: String, version: String, socket: String,toChatScreen: Bool?,userId: Int,opponentUserName: String?) -> UIViewController {
-        FayvKeys.ChatDefaults.chatToken = chatToken
-        FayvKeys.ChatDefaults.token = accessToken
-        FayvKeys.ChatDefaults.endpoint = appEndPoint
-        FayvKeys.ChatDefaults.socketUrl = socket
-        
-        FayvKeys.APIDefaults.baseUrl = appEndPoint
-        FayvKeys.APIDefaults.version = version
-        let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
-        if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
-          viewController.opponentUserName = opponentUserName
-          viewController.userId = userId
-            viewController.toChatScreen = toChatScreen!
-            
-            return viewController
-        }
-         return UIViewController()
-       }
     
-    public func presentInContainerWith(accessToken: String, chatToken: String, appEndPoint: String, version: String, socket: String, userId: Int,opponentUserName: String?) -> UIViewController {
+    public func openChatList(accessToken: String, chatToken: String, appEndPoint: String, version: String, socket: String) -> UIViewController {
         FayvKeys.ChatDefaults.chatToken = chatToken
         FayvKeys.ChatDefaults.token = accessToken
         FayvKeys.ChatDefaults.endpoint = appEndPoint
         FayvKeys.ChatDefaults.socketUrl = socket
-        
         FayvKeys.APIDefaults.baseUrl = appEndPoint
         FayvKeys.APIDefaults.version = version
+        
         let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
         if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? ConversationsViewController {
-          viewController.opponentUserName = opponentUserName
-          viewController.userId = userId
+            viewController.toChatScreen = false
             
             return viewController
         }
-         return UIViewController()
-       }
+        return UIViewController()
+    }
+    
+    
+    public func openChatMessage(accessToken: String, chatToken: String, appEndPoint: String, version: String, socket: String,toChatScreen: Bool?,userId: Int,opponentUserName: String?) -> UIViewController {
+        FayvKeys.ChatDefaults.chatToken = chatToken
+        FayvKeys.ChatDefaults.token = accessToken
+        FayvKeys.ChatDefaults.endpoint = appEndPoint
+        FayvKeys.ChatDefaults.socketUrl = socket
+        FayvKeys.APIDefaults.baseUrl = appEndPoint
+        FayvKeys.APIDefaults.version = version
+        
+        let storyboard = UIStoryboard(name: TechjaysChatIdentifiers.Storyboard.conversations.rawValue, bundle: Bundle.module)
+        if let viewController = storyboard.instantiateViewController(withIdentifier: TechjaysChatIdentifiers.ViewController.conversations.rawValue) as? MessagesViewController {
+            viewController.opponentUserName = opponentUserName
+            viewController.to_user_id = userId
+            viewController.toChatScreen = true
+            
+            return viewController
+        }
+        return UIViewController()
+    }
+    
 }
 
 
