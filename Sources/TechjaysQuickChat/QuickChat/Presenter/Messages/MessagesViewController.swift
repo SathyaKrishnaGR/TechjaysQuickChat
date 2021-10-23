711c6e54ecd850a5d7ebdfc10c10c79617fc4375
SathyaKrishna <sathya.gr@iderize.in>
2021-10-24 01:49:44 +0530

    self added

--- Sources/TechjaysQuickChat/QuickChat/Presenter/Messages/MessagesViewController.swift
+++ Sources/TechjaysQuickChat/QuickChat/Presenter/Messages/MessagesViewController.swift
@@ -196,7 +196,7 @@ extension MessagesViewController {
     }
     fileprivate func deleteAndRemoveRows(rows: [IndexPath], messages: [ObjectMessage], deleteType: String) {
         self.tableView.beginUpdates()
-        messages.removeArrayOfIndex(array: rows, completionHandler: {
+        self.messages.removeArrayOfIndex(array: rows, completionHandler: {
         })
         self.tableView.deleteRows(at: rows, with: .automatic)
         self.deleteChatMessages(rows: rows, messageIdToDelete: messages, deleteType: deleteType)
