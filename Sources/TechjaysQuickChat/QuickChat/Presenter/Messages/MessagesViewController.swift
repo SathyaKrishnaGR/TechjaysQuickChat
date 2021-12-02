0ec4bd37269e307e90452537bf86dd1a75bce6e2
Mahendran P <mahendran.p@techjays.com>
2021-12-02 19:25:18 +0530

    MessageVC bugfix

--- Sources/TechjaysQuickChat/QuickChat/Presenter/Messages/MessagesViewController.swift
+++ Sources/TechjaysQuickChat/QuickChat/Presenter/Messages/MessagesViewController.swift
@@ -352,22 +352,17 @@ extension MessagesViewController {
         APIClient().GET(url: url, headers: ["Authorization": FayvKeys.ChatDefaults.token]) { (status, response: APIResponse<[ObjectMessage]>) in
             switch status {
             case .SUCCESS:
+                self.messages.removeAll()
                 if let data = response.data {
                     if isFirstPage {
                         self.messages = data
                     } else {
                         self.messages.append(contentsOf: data )
                     }
-                    if let message = response.data {
-                        if message.count > 1 {
-                            self.messages = self.messages.sorted(by: {$0.timestamp?.stringToDate().compare(($1.timestamp?.stringToDate())!) == .orderedAscending})
-                        }
-                    }
-                 /*   if self.messages.count > 1 {
+                   
+                    if self.messages.count > 1 {
                         self.messages = self.messages.sorted(by: {$0.timestamp?.stringToDate().compare(($1.timestamp?.stringToDate())!) == .orderedAscending})
-                        
-                    }*/
-                    
+                    }
                     self.tableView.reloadData()
                     self.tableView.scroll(to: .bottom, animated: true)
                 }
