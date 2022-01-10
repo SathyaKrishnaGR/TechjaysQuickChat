1db849a085dec8ce1ec116f53e507e08a0e3ac81
SathyaKrishnaGR <69797138+SathyaKrishnaGR@users.noreply.github.com>
2022-01-10 22:39:39 +0530

    sorted

--- Sources/TechjaysQuickChat/QuickChat/Presenter/Messages/MessagesViewController.swift
+++ Sources/TechjaysQuickChat/QuickChat/Presenter/Messages/MessagesViewController.swift
@@ -369,6 +369,10 @@ extension MessagesViewController {
                     } else {
                         self.messages.append(contentsOf: data )
                     }
+                    if self.messages.count > 1 {
+                        self.messages = self.messages.sorted(by: {$0.timestamp?.stringToDate().compare(($1.timestamp?.stringToDate())!) == .orderedAscending})
+                        
+                    }
                     DispatchQueue.main.async {
                         self.tableView.reloadData()
                         self.tableView.scroll(to: .bottom, animated: true)
